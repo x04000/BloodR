7be6f58db413294ed3b26861a2986d2db260a8f5
x04000 <78761999+x04000@users.noreply.github.com>
2023-03-14 22:30:53 +0100

    Add files via upload

--- BloodR.lua
+++ BloodR.lua
@@ -14,7 +14,7 @@ local Window = Rayfield:CreateWindow({
       Subtitle = "Key System",
       Note = "https://github.com/x04000/README.md",
       FileName = "BloodRKey",
-      SaveKey = true,
+      SaveKey = false,
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = "12shadows"
    }
@@ -36,6 +36,62 @@ local Button = Tab:CreateButton({
    end,
 })
 
+-- General
+local Tab = Window:CreateTab("General", 4483362458) -- Title, Image
+
+local Button = Tab:CreateButton({
+   Name = "Inject InfiniteYield",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Inject Dex",
+   Callback = function()
+      loadstring(game:HttpGet("https://gist.githubusercontent.com/DinosaurXxX/b757fe011e7e600c0873f967fe427dc2/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4"))()
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Inject General Aimbot (With FOV circle)",
+   Callback = function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/ygp8Enye"))()
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Inject General Aimbot (Without FOV circle)",
+   Callback = function()
+      loadstring(game:HttpGet("https://pastebin.com/raw/CwQcEnEd"))()
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Inject General Godmode",
+   Callback = function()
+      loadstring(game:HttpGet("https://pastebin.com/4rqXL7jh"))()
+   end,
+})
+
+-- Hubs
+local Tab = Window:CreateTab("Hubs", 4483362458) -- Title, Image
+
+local Button = Tab:CreateButton({
+   Name = "Inject Orca",
+   Callback = function()
+      loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua"))()
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Inject SyctheHubV1",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/LaeraLuzy/SyctheHubV1/main/script.lua"))()
+   end,
+})
+local Button = Tab:CreateButton({
+   Name = "Inject Vynixius",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
+   end,
+})
+
 -- Jailbreak
 local Tab = Window:CreateTab("Jailbreak", 4483362458) -- Title, Image
 
@@ -51,24 +107,30 @@ local Button = Tab:CreateButton({
       loadstring(game:HttpGet("https://raw.githubusercontent.com/KuriWasTaken/MonkeyScripts/main/JailMonkey.lua"))()
    end,
 })
+local Button = Tab:CreateButton({
+   Name = "Inject Diamond",
+   Callback = function()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/BloxiYT/Diamond/main/JailBreak"))()
+   end,
+})
 
 -- Frontlines
 local Tab = Window:CreateTab("Frontlines", 4483362458) -- Title, Image
 
 local Button = Tab:CreateButton({
-   Name = "Inject Frontlines Hack (Low)",
+   Name = "Inject Frontlines ESP (Low)",
    Callback = function()
       loadstring(game:HttpGet("https://raw.githubusercontent.com/x04000/BloodR/main/Frontlines/Frontlines.lua"))()
    end,
 })
 local Button = Tab:CreateButton({
-   Name = "Inject Frontlines Hack (Medium)",
+   Name = "Inject Frontlines ESP (Medium)",
    Callback = function()
       loadstring(game:HttpGet("https://raw.githubusercontent.com/x04000/BloodR/main/Frontlines/Frontlines2.lua"))()
    end,
 })
 local Button = Tab:CreateButton({
-   Name = "Inject Frontlines Hack (Full)",
+   Name = "Inject Frontlines ESP (Full)",
    Callback = function()
       loadstring(game:HttpGet("https://raw.githubusercontent.com/x04000/BloodR/main/Frontlines/Frontlines3.lua"))()
    end,
