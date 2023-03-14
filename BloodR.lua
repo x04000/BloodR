6d46b4b14512a9f3363eb5322b41c2ca4271a7cd
x04000 <78761999+x04000@users.noreply.github.com>
2023-03-14 18:02:33 +0100

    Update BloodR.lua

--- BloodR.lua
+++ BloodR.lua
@@ -14,7 +14,7 @@ local Window = Rayfield:CreateWindow({
       Subtitle = "Key System",
       Note = "https://github.com/x04000/README.md",
       FileName = "BloodRKey",
-      SaveKey = false,
+      SaveKey = true,
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = "12shadows"
    }
